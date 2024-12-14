ad640944260368b3fbe4490ffe9accde89e0d522
raksha-rajpoot <rakraj654@gmail.com>
2024-12-14 17:41:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ function isSameType(value1, value2) {
 if (Number.isNaN(value1) && Number.isNaN(value2)) {
     return true;
   }
+	else{
+		return false;
+	}
   return typeof value1 === typeof value2;
 }
 // }	
